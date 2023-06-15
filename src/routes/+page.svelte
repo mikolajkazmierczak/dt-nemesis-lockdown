9db1d691ae9e240f9a3a72a0f993ae3b99c3f071
Mikołaj Kaźmierczak <mikolaj@kazmierczak.pl>
2023-06-15 13:48:09 +0200

    ultra simple demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
 <h1>Nemesis: Lockdown</h1>
-<p>Cicho tu... zbyt cicho.</p>
+
+<p id="qwe">
+	Połóżcie <a href="#asd">planszę główną</a> na stole stroną podstawową ku górze (jak pokazano na ilustracji)
+</p>
+
+<div id="asd">Plansza główna to element gry na którym układa się elementy gry.</div>
+
+<style>
+	[id] {
+		margin-bottom: 100vh;
+	}
+</style>
